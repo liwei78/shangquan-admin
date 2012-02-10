25c323053e91c10b80e58feccae16d95e53605e0
liwei <hi@liwei.me>
2012-02-10 14:17:41 +0800

    fix

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,7 @@
 class Activity < ActiveRecord::Base
   validates :title, :presence => true
+  has_attached_file :poster,
+    :styles => { :original => "600>", :small => "120>" },
+    :url => "/:class/:attachment/:id/:style_:basename.:extension",
+    :default_url => "nopic.jpg"
 end
