4ec3516bf7694945ab3df0046021dbb327d6c96d
liwei <hi@liwei.me>
2012-02-15 00:58:46 +0800

    fix

--- db/migrate/20120112014308_create_goods.rb
+++ db/migrate/20120112014308_create_goods.rb
@@ -1,10 +1,10 @@
 class CreateGoods < ActiveRecord::Migration
   def change
     create_table :goods do |t|
-      t.string  :title
-      t.decimal :price, :precision => 8, :scale =>2, :default=>0.00
-      t.integer :brand_id
-      t.integer :likes_count
+      t.string   :title
+      t.decimal  :price, :precision => 8, :scale =>2, :default=>0.00
+      t.integer  :brand_id
+      t.integer  :likes_count
       t.string   :poster_file_name
       t.string   :poster_content_type
       t.integer  :poster_file_size
--- db/migrate/20120214074726_create_brands.rb
+++ db/migrate/20120214074726_create_brands.rb
@@ -1,10 +1,10 @@
 class CreateBrands < ActiveRecord::Migration
   def change
     create_table :brands do |t|
-      t.string  :title
+      t.string   :title
       t.integer  :nation
-      t.string  :letter
-      t.integer :category_id
+      t.string   :letter
+      t.integer  :category_id
       t.string   :poster_file_name
       t.string   :poster_content_type
       t.integer  :poster_file_size
